04b33a1fe1144d85ec3ac19fb2032d9a2418da05
GabrielRuiseco19170012 <19170012@uttcampus.edu.mx>
2021-08-09 12:45:40 -0500

    configuraciones

--- config/database.ts
+++ config/database.ts
@@ -7,8 +7,8 @@
 
 import Env from '@ioc:Adonis/Core/Env'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
-// const Url = require('url-parse')
-// const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+import Url from ‘url-parse’
+const CLEARDB_DATABASE_URL = new Url(Env.get(‘CLEARDB_DATABASE_URL’))
 
 
 const databaseConfig: DatabaseConfig = {
@@ -39,17 +39,13 @@ const databaseConfig: DatabaseConfig = {
     mysql: {
       client: 'mysql',
       connection: {
-        host: Env.get('MYSQL_HOST'),
-        port: Env.get('MYSQL_PORT'),
-        user: Env.get('MYSQL_USER'),
-        password: Env.get('MYSQL_PASSWORD', ''),
-        database: Env.get('MYSQL_DB_NAME'),
-      },
-      migrations: {
-        naturalSort: true,
+        host: CLEARDB_DATABASE_URL.host as string,
+        port: Number(''),
+        user: CLEARDB_DATABASE_URL.username as string,
+        password: CLEARDB_DATABASE_URL.password as string,
+        database: CLEARDB_DATABASE_URL.pathname.substr(1) as string
       },
       healthCheck: false,
-      debug: false,
     },
     /*
   |--------------------------------------------------------------------------
