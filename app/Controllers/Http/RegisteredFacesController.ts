fd2a0202c5403ab52c90d834c2c619db6b0e274a
GabrielRuiseco19170012 <19170012@uttcampus.edu.mx>
2021-08-14 13:49:10 -0500

    deploy

--- app/Controllers/Http/RegisteredFacesController.ts
+++ app/Controllers/Http/RegisteredFacesController.ts
@@ -260,14 +260,14 @@ export default class RegisteredFacesController {
     }
   }
 
-  public async mongoIndex({ request, response }) {
+  public async mongoIndex({ response }) {
     try {
       mongoose.connect(Env.get('MONGO_CONNECTION_STRING'), {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
-      const { id } = request.only(['id'])
-      const data = await Person.find({ "idUser": id.toString() })
+      // const { id } = request.only(['id'])
+      const data = await Person.find({ idUser: '5' })
       return response.status(200).json(data)
     } catch (e) {
       return response.status(400).send({ Error: e })
